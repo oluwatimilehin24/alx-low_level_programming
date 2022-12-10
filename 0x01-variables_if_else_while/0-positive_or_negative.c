b241bcfa4c65a6506e1ecb0c9ba9166ccc525c85
Oluwatimilehin24 <obasanyatimilehin24@outlook.com>
2022-12-09 16:16:47 -0800

    if else while

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,10 +16,10 @@ int main(void)
 
 	if (n > 0)
 		printf("%i is positive\n", n);
-	else if(n < 0)
+	else if (n < 0)
 		printf("%i is negative\n", n);
 	else
 		printf("%i is zero\n", n);
 
-	return(0);
+	return (0);
 }
