179002fbfc462bafe2d7e797fb05935f618d15e9
Oluwatimilehin24 <obasanyatimilehin24@outlook.com>
2022-12-09 18:42:43 -0800

    if-else-elseif

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,7 +10,7 @@ int main(void)
 	int i, j;
 	int a, b, c, d;
 
-	for (i < 0; i < 100; i++)
+	for (i = 0; i < 100; i++)
 	{
 		a = i / 10;
 		b = i % 10;
@@ -20,7 +20,7 @@ int main(void)
 			c = j / 10;
 			d = j % 10;
 
-			if (a < c || (a == c b < d))
+			if (a < c || (a == c && b < d))
 			{
 				putchar(a + '0');
 				putchar(b + '0');
