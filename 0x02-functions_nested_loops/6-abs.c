51808078f43d1a7aba4ca846ee427f312c7671c4
Oluwatimilehin24 <obasanyatimilehin24@outlook.com>
2022-12-15 15:08:31 -0800

    functions nested loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - compute the absolute value of an integer
+ * @n: int type number
+ * Return: absolute value of @n
+ */
+int abs(int n)
+{
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	else
+	{
+		return (n);
+	}
+}
