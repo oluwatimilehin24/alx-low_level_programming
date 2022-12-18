52b6e2f80e6b4394d0dc4b496d0fbc125171c0d1
Oluwatimilehin24 <obasanyatimilehin24@outlook.com>
2022-12-18 14:48:13 -0800

    more functions nested loops

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,16 +3,15 @@
 /**
  * print_numbers - Print numbers 0 to 9, followed by a new line
  * Description: Can only use _putchar twice
- * Rettur: Always 0
+ * Return: Always 0
  */
-
 void print_numbers(void)
 {
 	int i;
 
 	while (i = '0'; i <= '9', i++)
 	{
-	       _putchar(i + '0');	       
+	       _putchar(i);	       
 	}
 	_putchar('\n');
 }
