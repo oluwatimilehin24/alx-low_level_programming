493f6b889a7512b15b70f3930886da28da39af6d
Oluwatimilehin24 <obasanyatimilehin24@outlook.com>
2022-12-19 16:50:27 -0800

    pointers arrays and strings

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#innclude "main.h"
+
+/**
+ * _puts - print a string stdout
+ * @str: char array string type
+ * Description: Can only use _putchar
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
