e6ab8bf2317064e3a6e3667a6ea9f4bc71468054
Oluwatimilehin24 <obasanyatimilehin24@outlook.com>
2023-01-10 18:41:45 -0800

    malloc free

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -25,13 +25,13 @@ int **alloc_grid(int width, int height)
 		grid[i] = malloc(width * sizeof(int));
 		if (grid[i] == NULL)
 		{
-			while (i >= o)
+			while (i >= 0)
 			{
 				free(grid[i]);
 				i--;
 			}
 			free(grid);
-			return (NULL):
+			return (NULL);
 		}
 	}
 	return (grid);
