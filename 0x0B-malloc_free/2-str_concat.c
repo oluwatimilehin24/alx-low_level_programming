c989668829653672fac5276e6a756be4105b0e08
Oluwatimilehin24 <obasanyatimilehin24@outlook.com>
2023-01-10 18:20:25 -0800

    malloc free

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,11 +15,11 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL)
 		s1 = "";
-	if (s2 ++ NULL)
+	if (s2 == NULL)
 		s2 = "";
 
 	length1 = 0;
-	while (*(s1 + length) != '\0')
+	while (*(s1 + length1) != '\0')
 	{
 		length1++;
 	}
